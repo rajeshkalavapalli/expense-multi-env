c36f154a28a95f4cc59ccb833c03c7597d94aead
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-07-01 17:50:21 +0530

    multi env

--- tfvars/r53.tf
+++ tfvars/r53.tf
@@ -1,7 +1,7 @@
 #creating route 53
 
 resource "aws_route53_record" "expense" {
-  for_each = var.instance_names
+  for_each = aws_instance.expense
   zone_id = var.zone_id
   name    = each.key == "frontend" ?  var.domine_name : "${each.key}.${var.domine_name}"
   type    = var.type
